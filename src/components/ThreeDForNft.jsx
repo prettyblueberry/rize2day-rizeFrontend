ef2639a828f2baa27c821db460b6d5c1de0186be
thr2240 <thomasr2240@gmail.com>
2023-05-10 15:44:08 +0900

    updated ThreeDForNft.jsx

--- src/components/ThreeDForNft.jsx
+++ src/components/ThreeDForNft.jsx
@@ -1,57 +1,12 @@
-import { useRef, useState, Suspense } from "react";
+import { useState } from "react";
 import { useAppSelector } from "app/hooks";
-import { Canvas, useLoader, useFrame } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { OrbitControls, useProgress, Html } from "@react-three/drei";
 import { ErrorBoundary } from "react-error-boundary";
 import { selectCurrentMediaRunning } from "app/mediaRunning/mediaRunning";
 import { BiFullscreen } from "react-icons/bi";
 import { IconButton } from "@mui/material";
 import NcModal from "shared/NcModal/NcModal";
-import * as THREE from "three";
-
-const Loader = () => {
-  const { active, progress, errors, item, loaded, total } = useProgress();
-
-  console.log("progress ===> ", progress);
-
-  return (
-    <Html className="text-black" center>
-      {progress} % loaded
-    </Html>
-  );
-};
-
-const ThreeDForNft = ({ src = "" }) => {
-  let nscene;
-  const mixerRef = useRef();
-
-  useFrame((_, delta) => {
-    mixerRef.current.update(delta);
-  });
-
-  const { scene, animations } = useLoader(GLTFLoader, src);
-  nscene = scene;
-  const mixer = new THREE.AnimationMixer(scene);
-  mixerRef.current = mixer;
-  animations.forEach((clip) => {
-    const action = mixer.clipAction(clip);
-    action.play();
-  });
-
-  return (
-    <>
-      <ambientLight intensity={0.6} color={"0xffffff"} />
-      <pointLight position={[10, 10, 10]} />
-
-      <Suspense fallback={<Loader />}>
-        <primitive object={nscene} />
-      </Suspense>
-
-      <OrbitControls />
-    </>
-  );
-};
+import ReactLoading from 'react-loading';
+import bgImg from '../images/3dbg.jpg';
 
 const NFT3Canvas = ({
   nftId,
@@ -69,9 +24,8 @@ const NFT3Canvas = ({
   const renderContent = (newClass = "", content = true) => {
     return (
       <div
-        className={`${className} ${newClass} ${
-          IS_PLAY ? "" : "opacity-0 z-[-1]"
-        }`}
+        className={`${className} ${newClass} ${IS_PLAY ? "" : "opacity-0 z-[-1]"
+          }`}
       >
         <ErrorBoundary
           FallbackComponent={({ error }) => (
@@ -80,15 +34,25 @@ const NFT3Canvas = ({
             </div>
           )}
         >
-          <Canvas
-            camera={{
-              position: [0, 0, 2],
-              fov: 55,
-            }}
-            className={`${className}`}
-          >
-            {content && <ThreeDForNft src={src} />}
-          </Canvas>
+          {content && (
+            <model-viewer
+              src={src}
+              skybox-image={bgImg}
+              camera-controls
+              shadow-intensity="1"
+              autoplay
+              ar
+              ar-modes="webxr scene-viewer quick-look"
+              ar-status="not-presenting"
+              interaction-prompt="none"
+              alt="An animated 3D model of a robot"
+              style={{ width: '100%', height: '100%' }}
+            >
+              <div className="model-viewer-loader" slot="poster">
+                <ReactLoading type="bubbles" color="#fff" />
+              </div>
+            </model-viewer>
+          )}
         </ErrorBoundary>
       </div>
     );
